51dad1d4ad4c15a3c2d6b0c0610c29facda81878
Josh <joshwestbury@gmail.com>
2019-09-30 21:51:52 -0500

    add theme and NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Heading, Grommet } from 'grommet';
+import NavBar from './Components/NavBar';
+
+const theme = {
+    global: {
+        colors: {
+            brand: '#228BE6'
+        },
+        font: {
+            family: 'Roboto',
+            size: '14px',
+            height: '20px'
+        }
+    }
+};
+
+// const NavBar = (props: any) => (
+//     <Box
+//         tag="header"
+//         direction="row"
+//         align="center"
+//         justify="between"
+//         background="brand"
+//         pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+//         elevation="medium"
+//         style={{ zIndex: '1' }}
+//         {...props}
+//     />
+// );
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <Grommet theme={theme} full>
+            <Box fill>
+                <NavBar>
+                    <Heading level="3" margin="none">
+                        Visualize Bits and Bytes
+                    </Heading>
+                </NavBar>
+                <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+                    <Box flex align="center" justify="center">
+                        app body
+                    </Box>
+                </Box>
+            </Box>
+        </Grommet>
+    );
+};
 
 export default App;
