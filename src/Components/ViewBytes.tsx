66beb3c1066fdb898f9e751c6463da10d1232ba6
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2019-10-10 10:47:00 -0700

    Use FixedSizeGrid.

--- src/Components/ViewBytes.tsx
+++ src/Components/ViewBytes.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { FixedSizeList } from 'react-window';
+import { FixedSizeGrid } from 'react-window';
 
 
 export default function ViewBytes({ bytes }: any) {
-    const Row = ({ index, style } : any) => (
-      <div style={style}>Byte {index}: {bytes[index]}</div>
-    );
+    const Cell = ({ columnIndex, rowIndex, style } : any) => {
+      if (rowIndex === 0 && columnIndex === 0) {
+        return (<div style={style}><b>Byte</b></div>);
+      } else if (rowIndex === 0 && columnIndex === 1) {
+        return (<div style={style}><b>Value</b></div>);
+      } else if (columnIndex === 0) {
+        return (<div style={style}>{rowIndex - 1}</div>);
+      } else {
+        return (<div style={style}>{bytes[rowIndex - 1]}</div>);
+      }
+    };
 
     return (
         <div>
-          <FixedSizeList
+          <FixedSizeGrid
+            columnCount={2}
+            columnWidth={80}
             height={400}
-            itemCount={bytes.length}
-            itemSize={22}
+            rowCount={bytes.length + 1}
+            rowHeight={22}
             width={300}
             >
-            {Row}
-          </FixedSizeList>
+            {Cell}
+          </FixedSizeGrid>
         </div>
     );
 }
